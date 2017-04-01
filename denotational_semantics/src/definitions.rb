5ca073fa6c0d5ccedb833bfd0f27941e2e3e3a4c
Yohei Okubo <okubo.yohei@gmail.com>
2017-04-02 01:25:29 +0900

    to lambda method

--- denotational_semantics/src/definitions.rb
+++ denotational_semantics/src/definitions.rb
@@ -1,13 +1,17 @@
 require '../../base_definitions.rb'
 
+def to_lambda_str(_inspect)
+  "-> e { #{_inspect} }"
+end
+
 class Number # args => {:value}
   def to_ruby
-    "-> e { #{value.inspect} }"
+    to_lambda_str value.inspect
   end
 end
 
 class Boolean
   def to_ruby
-    "-> e { #{value.inspect} }"
+    to_lambda_str value.inspect
   end
 end
